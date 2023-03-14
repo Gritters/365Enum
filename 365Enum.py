dd656a4701629fbf72d8d92336230b5f7fda4fb1
Gritters <cameronhomer17@gmail.com>
2023-03-14 13:29:09 -0600

    First Commit

---
+++ 365Enum.py
@@ -0,0 +1,27 @@
+import os
+import requests
+import argparse
+
+
+
+
+def printInfo(response):
+    
+    responseDict = response.json()
+    print("--------")
+    print("Domain Type")
+    print("--------")
+    if responseDict.get("NameSpaceType") == "Unknown":
+        print("This domain does not exist in M365")
+    elif responseDict.get("NameSpaceType") == "Federated":
+        print(f"{domain} is a Federated domain")
+    elif responseDict.get("NameSpaceType") == "Managed":
+        print(f"{domain} is a Managed domain")
+    return
+
+domain = input("What is the domain you would like to enumerate? \n")
+url = 'https://login.microsoftonline.com/getuserrealm.srf?login='
+fullUrl = url + domain 
+
+r = requests.get(fullUrl)
+printInfo(r)
